0792687c1f1caed470243e7cd2ac8259c1cfa685
Rodrigo Chang <rrcp777@gmail.com>
2025-02-18 10:25:50 -0600

    Add new 2024 CPI base

--- src/CPIDataGT.jl
+++ src/CPIDataGT.jl
@@ -17,14 +17,15 @@ module CPIDataGT
     export GT00, GT10               # VarCPIBase with month-to-month IPC variations
     export FGT00, FGT10             # FullCPIBase with complete IPC data (codes, names)
     export GTDATA                   # CountryStructure wrapper
-    export CPITREE00, CPITREE10, CPITREE23     # CPI hierarchical tree structures
+    export CPITREE00, CPITREE10, CPITREE23, CPITREE24   # CPI hierarchical tree structures
 
     # Experimental
     export FGT23, GT23, GTDATA23
+    export FGT24, GT24, GTDATA24
 
     ## DataFrames objects
-    export DF_ITEMS_00, DF_ITEMS_10, DF_ITEMS_23
-    export DF_CPI_00, DF_CPI_10, DF_CPI_23
+    export DF_ITEMS_00, DF_ITEMS_10, DF_ITEMS_23, DF_ITEMS_24
+    export DF_CPI_00, DF_CPI_10, DF_CPI_23, DF_CPI_24
 
     ## Functions to build and load data 
     export load_data, load_tree_data, load_dataframes
@@ -33,8 +34,8 @@ module CPIDataGT
     PROJECT_ROOT = pkgdir(@__MODULE__)
     # JLD files directory (filled by Scratch)
     JLD_DIRECTORY = ""
-    # URLs to download 2023 data
-    include("CPI2023_urls.jl")
+    # URLs to download 2024 data
+    include("current_CPI_urls.jl")
 
     # Access non-changing CSV files
     datadir(file) = joinpath(PROJECT_ROOT, "data", file)
@@ -56,7 +57,7 @@ module CPIDataGT
     """
         load_data(; full_precision = false)
 
-    Load the data from the main CPI data file defined in `MAIN_DATAFILE` 
+    Load the data from the current CPI data file defined in `MAIN_DATAFILE` 
     with 32-bit precision. 
     - The option `full_precision` allows loading the data with 64-bit precision.
     """
@@ -70,16 +71,26 @@ module CPIDataGT
         @info "Loading Guatemalan CPI data..."
         global FGT00, FGT10, GT00, GT10, GTDATA = load(datafile, "fgt00", "fgt10", "gt00", "gt10", "gtdata")
         @info "Data loaded in exported structures `FGT00`, `FGT10`, `GT00`, `GT10` and `GTDATA`"
-        global FGT23, GT23, GTDATA23 = load(datafile, "fgt23", "gt23", "gtdata_exp")
-        @info "Experimental new data on `FGT23`, `GT23`, and `GTDATA23`"
+        global FGT23, GT23, GTDATA23 = load(datafile, "fgt23", "gt23", "gtdata_23")
+        @info "CPI 2023 data on `FGT23`, `GT23`, and `GTDATA23`"
+        global FGT24, GT24, GTDATA24 = load(datafile, "fgt24", "gt24", "gtdata_24")
+        @info "CPI 2024 data on `FGT24`, `GT24`, and `GTDATA24`"
     end
 
     """
         load_dataframes(; full_precision = false)
 
     Load the DataFrames from the building process: 
-    - DataFrames with information from the CPI items: `DF_ITEMS_00`, `DF_ITEMS_10`, `DF_ITEMS_23`.
-    - DataFrames with index numbers: `DF_CPI_00`, `DF_CPI_10`, `DF_CPI_23`.
+    - DataFrames with information from the CPI items: 
+        - `DF_ITEMS_00`, 
+        - `DF_ITEMS_10`, 
+        - `DF_ITEMS_23`, 
+        - `DF_ITEMS_24`, 
+
+    - DataFrames with index numbers: 
+        - `DF_CPI_00`, 
+        - `DF_CPI_10`, 
+        - `DF_CPI_23`.
     """
     function load_dataframes() 
         datafile = DATAFRAMES_FILE
@@ -89,17 +100,22 @@ module CPIDataGT
 
         # Load data
         @info "Loading Guatemalan CPI DataFrames..."
-        global DF_ITEMS_00, DF_ITEMS_10, DF_ITEMS_23 = load(datafile, "gt00gb", "gt10gb", "gt23gb")
-        global DF_CPI_00, DF_CPI_10, DF_CPI_23 = load(datafile, "gt_base00", "gt_base10", "gt_base23")
-        @info "Items DataFrames loaded: `DF_ITEMS_00`, `DF_ITEMS_10`, `DF_ITEMS_23`" 
-        @info "Index numbers DataFrames loaded: `DF_CPI_00`, `DF_CPI_10`, `DF_CPI_23`"
+        global DF_ITEMS_00, DF_ITEMS_10, DF_ITEMS_23, DF_ITEMS_24 = load(datafile, "gt00gb", "gt10gb", "gt23gb", "gt24gb")
+        global DF_CPI_00, DF_CPI_10, DF_CPI_23, DF_CPI_24 = load(datafile, "gt_base00", "gt_base10", "gt_base23", "gt_base24")
+        @info "Items DataFrames loaded: `DF_ITEMS_00`, `DF_ITEMS_10`, `DF_ITEMS_23` and `DF_ITEMS_24`" 
+        @info "Index numbers DataFrames loaded: `DF_CPI_00`, `DF_CPI_10`, `DF_CPI_23` and `DF_CPI_24`"
     end
 
     """
         load_tree_data(; full_precision = false)
 
-    Load the hierarchical CPI trees into the variables `CPITREE00` and
-    `CPITREE10`. The option `full_precision` allows loading the data with
+    Load the hierarchical CPI trees into the variables: 
+    - `CPITREE00`,
+    - `CPITREE10`,
+    - `CPITREE23`,
+    - `CPITREE24`,
+
+    The option `full_precision` allows loading the data with
     64-bit precision.
     """
     function load_tree_data(; full_precision::Bool = false) 
@@ -110,14 +126,15 @@ module CPIDataGT
 
         # Load data
         @info "Loading the Guatemalan CPI hierarchical tree data..."
-        global CPITREE00, CPITREE10, CPITREE23 = load(datafile, "cpi_00_tree", "cpi_10_tree", "cpi_23_tree")
-        @info "Data loaded in exported consts `CPITREE00`, `CPITREE10` and `CPITREE23`"
+        global CPITREE00, CPITREE10, CPITREE23, CPITREE24 = load(datafile, "cpi_00_tree", "cpi_10_tree", "cpi_23_tree", "cpi_24_tree")
+        @info "Data loaded in exported consts `CPITREE00`, `CPITREE10`, `CPITREE23` and `CPITREE24`"
     end
 
 
     """ 
         update_data()
-    Downloads CSV files from Google Drive and updates the 2023 CPI data files.
+
+    Downloads CSV files from Google Drive and updates the 2024 CPI data files.
     """ 
     function update_data() 
         @info "Downloading 2024 CPI data..."
@@ -143,22 +160,16 @@ module CPIDataGT
         # 2010 Base
         gt_base10 = CSV.read(datadir("Guatemala_IPC_2010.csv"), DataFrame, normalizenames=true)
         gt10gb = CSV.read(datadir("Guatemala_GB_2010.csv"), DataFrame, types=[String, String, Float64])
-        # 2024 Base
+        # 2023 Base
+        gt_base23 = CSV.read(datadir("Guatemala_IPC_2023.csv"), DataFrame, normalizenames=true)
+        gt23gb = CSV.read(datadir("Guatemala_GB_2023.csv"), DataFrame, types=[String, String, Float64])
+
+        # Current 2024 Base
         newdatafile = newdatadir("Guatemala_IPC_2024.csv")
         isfile(newdatafile) || update_data()
-        gt_base23 = CSV.read(newdatafile, DataFrame, normalizenames=true)
-        gt23gb = CSV.read(newdatadir("Guatemala_GB_2024.csv"), DataFrame, types=[String, String, Float64])
+        gt_base24 = CSV.read(newdatafile, DataFrame, normalizenames=true)
+        gt24gb = CSV.read(newdatadir("Guatemala_GB_2024.csv"), DataFrame, types=[String, String, Float64])
         @info "Data successfully loaded from CSV files"
-
-        # Fix to handle different base indexes in 2024 CPI base:
-        #   Rebase individual indexes for the base period index to be 100.0
-        #   Need to force first row of the DataFrame to be = 100.0 due to imprecision of operations
-        #   This issue needs to be handled properly in CPIDataBase.jl 
-        matdata = gt_base23[:, 2:end] |> Matrix
-        newdata = 100 * matdata ./ reshape(matdata[1, :], 1, :)
-        gt_base23_mod = copy(gt_base23)
-        gt_base23_mod[:, 2:end] .= newdata
-        gt_base23_mod[1, 2:end] .= 100.0
 
         ## Building data structures
         # 2000 Base
@@ -174,23 +185,48 @@ module CPIDataGT
         var_gt10_32 = VarCPIBase(full_gt10_32)
 
         # 2023 Base
-        full_gt23_64 = FullCPIBase(gt_base23_mod, gt23gb)
+        full_gt23_64 = FullCPIBase(gt_base23, gt23gb)
         full_gt23_32 = convert(Float32, full_gt23_64)
         var_gt23_64 = VarCPIBase(full_gt23_64)
         var_gt23_32 = VarCPIBase(full_gt23_32)
 
-        # Country data container structure
+        # 2024 Base
+        full_gt24_64 = FullCPIBase(gt_base24, gt24gb)
+        full_gt24_32 = convert(Float32, full_gt24_64)
+        var_gt24_64 = VarCPIBase(full_gt24_64)
+        var_gt24_32 = VarCPIBase(full_gt24_32)
+
+        # Country data container structure 2000-2010 CPI bases
         gtdata_32 = UniformCountryStructure(var_gt00_32, var_gt10_32)
         gtdata_64 = UniformCountryStructure(var_gt00_64, var_gt10_64)
 
-        # Experimental new data container
-        gtdata_32_exp = UniformCountryStructure(var_gt00_32, var_gt10_32, var_gt23_32)
-        gtdata_64_exp = UniformCountryStructure(var_gt00_64, var_gt10_64, var_gt23_64)
+        # Container for data including 2023 CPI base
+        gtdata23_32 = UniformCountryStructure(var_gt00_32, var_gt10_32, var_gt23_32)
+        gtdata23_64 = UniformCountryStructure(var_gt00_64, var_gt10_64, var_gt23_64)
 
-        @info "Successful building of data structures" GTDATA=gtdata_32 GTDATA23=gtdata_32_exp
+        # Experimental new data container including 2024 new CPI base
+        gtdata24_32 = MixedCountryStructure(var_gt00_32, var_gt10_32, var_gt23_32, var_gt24_32)
+        gtdata24_64 = MixedCountryStructure(var_gt00_64, var_gt10_64, var_gt23_64, var_gt24_64)
+
+        @info "Successful building of data structures" GTDATA=gtdata_32 GTDATA23=gtdata23_32 GTDATA24=gtdata24_32
 
         ## Build the hierarchical IPC tree Base 2023
-        groups23 = CSV.read(newdatadir("Guatemala_IPC_2024_Groups.csv"), DataFrame)
+        groups24 = CSV.read(newdatadir("Guatemala_IPC_2024_Groups.csv"), DataFrame)
+
+        cpi_24_tree_32 = CPITree(
+            base = full_gt24_32, 
+            groupsdf = groups24,
+            characters = (3, 4, 5, 6, 8),
+        )
+
+        cpi_24_tree_64 = CPITree(
+            base = full_gt24_64, 
+            groupsdf = groups24,
+            characters = (3, 4, 5, 6, 8),
+        )
+
+        ## Build the hierarchical IPC tree Base 2023
+        groups23 = CSV.read(newdatadir("Guatemala_IPC_2023_Groups.csv"), DataFrame)
 
         cpi_23_tree_32 = CPITree(
             base = full_gt23_32, 
@@ -243,17 +279,21 @@ module CPIDataGT
             fgt00 = full_gt00_32, 
             fgt10 = full_gt10_32, 
             fgt23 = full_gt23_32, 
+            fgt24 = full_gt24_32, 
             # VarCPIBase
             gt00 = var_gt00_32, 
             gt10 = var_gt10_32, 
             gt23 = var_gt23_32, 
+            gt24 = var_gt24_32, 
             # UniformCountryStructure
             gtdata = gtdata_32, 
-            gtdata_exp = gtdata_32_exp,
+            gtdata_23 = gtdata23_32,
+            gtdata_24 = gtdata24_32,
             # Hierarchical trees
             cpi_00_tree = cpi_00_tree_32, 
             cpi_10_tree = cpi_10_tree_32,
             cpi_23_tree = cpi_23_tree_32,
+            cpi_24_tree = cpi_24_tree_32,
         )
 
         jldsave(DOUBLE_DATAFILE; 
@@ -261,17 +301,21 @@ module CPIDataGT
             fgt00 = full_gt00_64, 
             fgt10 = full_gt10_64, 
             fgt23 = full_gt23_64, 
+            fgt24 = full_gt24_64, 
             # VarCPIBase
             gt00 = var_gt00_64, 
             gt10 = var_gt10_64, 
             gt23 = var_gt23_64, 
+            gt24 = var_gt24_64, 
             # UniformCountryStructure
             gtdata = gtdata_64, 
-            gtdata_exp = gtdata_64_exp,
+            gtdata_23 = gtdata23_64,
+            gtdata_24 = gtdata24_64,
             # Hierarchical trees
             cpi_00_tree = cpi_00_tree_64, 
             cpi_10_tree = cpi_10_tree_64,
             cpi_23_tree = cpi_23_tree_64,
+            cpi_24_tree = cpi_24_tree_64,
         )
 
         # Original DataFrames
@@ -282,6 +326,8 @@ module CPIDataGT
             gt_base10, gt10gb,
             # IPC base 2023
             gt_base23, gt23gb,
+            # IPC base 2024
+            gt_base24, gt24gb,
         )
 
         @info "Data structures successfully saved"
--- src/current_CPI_urls.jl
+++ src/current_CPI_urls.jl
@@ -1,13 +1,15 @@
+## From 2025, 2023 CPI base is fixed
+
 # Guatemala_IPC_2023_Groups.csv
-CPI2023_GROUPS_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQP5Z-PCddUAHHjSNx3CEEkKwtKnoaHramSEVkoIdpTV8b_ins0BbNqGlpMkhCdUXvjp5nr5LcBdFEm/pub?gid=1692561724&single=true&output=csv"
+# CPI2023_GROUPS_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQP5Z-PCddUAHHjSNx3CEEkKwtKnoaHramSEVkoIdpTV8b_ins0BbNqGlpMkhCdUXvjp5nr5LcBdFEm/pub?gid=1692561724&single=true&output=csv"
 
 # Guatemala_IPC_2023.csv
-CPI2023_INDEX_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQWMe_C3Vk7uM1NrTNLYHcl8tjKK56FjJk5mvyJj7mG1UjyOoHIqe1xhK1NxI6U8QURtH_uIUSFHGU_/pub?gid=1134661209&single=true&output=csv"
+# CPI2023_INDEX_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQWMe_C3Vk7uM1NrTNLYHcl8tjKK56FjJk5mvyJj7mG1UjyOoHIqe1xhK1NxI6U8QURtH_uIUSFHGU_/pub?gid=1134661209&single=true&output=csv"
 
 # Guatemala_GB_2023.csv
-CPI2023_ITEMS_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQKchrDZjeP08BseVw4VKY6aA7Ww5EkgyzZ_4RlcexpUIF9F1bhM9UzC139RcnNpwcDHYTAYR0AcOBs/pub?gid=126840596&single=true&output=csv"
+# CPI2023_ITEMS_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQKchrDZjeP08BseVw4VKY6aA7Ww5EkgyzZ_4RlcexpUIF9F1bhM9UzC139RcnNpwcDHYTAYR0AcOBs/pub?gid=126840596&single=true&output=csv"
 
-## CPI Change of Base Period 2024
+## From January 2025, there was a change of base period to the whole year 2024
 
 # Guatemala_IPC_2024_Groups.csv
 CPI2024_GROUPS_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRc9ONvYBDgMIqhM-bgVOgPQlUDqqulw2XtX0Nih2GFHuMo_b0BJl8ddQYbocWaVxPiEVRjTEtL9T9u/pub?gid=1692561724&single=true&output=csv"
